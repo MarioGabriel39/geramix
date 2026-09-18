3e5fe13678ba00026767796fdfb8ec7093ea5122
MarioGabriel39 <oquetemprahoje2016@gmail.com>
2026-09-18 16:12:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import fsp from "fs/promises";
 import path from "path";
 import os from "os";
 import crypto from "crypto";
+import cookieParser from "cookie-parser";
+import bcrypt from "bcryptjs";
 import { spawn } from "child_process";
 import archiver from "archiver";
 import ffmpegPath from "ffmpeg-static";
